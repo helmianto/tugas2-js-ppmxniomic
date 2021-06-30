5b27b30f6d653a29f04f3624e875c6070dd92069
Helmianto <helmianto15@gmail.com>
2021-06-30 16:12:22 +0700

    initial commit

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+const numberData = 24;
+const stringData = 'Halo saya helmianto';
+const booleanData = true;
+const nullData = null;
+const arrayData = ['Helmianto', 'Laki-laki', 23];
+
+console.log('Ini numberData', numberData);
+console.log('ini stringData', stringData);
+console.log('Ini booleanData', booleanData);
+console.log('Ini nullData', nullData);
+console.log('Ini arrayData', arrayData);
